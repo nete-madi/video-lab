3d6b530db0fcb5ec8135e3cbf9f84107f3dd1828
whatsupdawg <netemadi20@gmail.com>
2022-10-02 13:58:13 -0700

    removed a todo

--- index.py
+++ index.py
@@ -4,7 +4,6 @@ import os
 
 # TODO: Attempt to overlay a title card onto your clip.
 # TODO: Align text with form control boxes.
-# TODO: Allow a clip to upload with its filename if no name is specified.
 
 # https://stackoverflow.com/questions/72914568/overlay-image-on-video-using-moviepy
 # First: implement frontend for the functionality.
