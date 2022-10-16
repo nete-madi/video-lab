8afc863c9bc8a39a36bc6e32b7e5f6bea7e3b671
Nadia Etemadi <netemadi20@gmail.com>
2022-10-15 19:11:53 -0700

    refactoring to include blueprints

---
+++ editing/routes.py
@@ -0,0 +1,85 @@
+from flask import Flask, request, render_template, send_file, Blueprint
+from editing.video_utils import *
+import os
+
+editing = Blueprint('editing', __name__, template_folder='templates')
+video_save_path = "\\clips\\"
+
+
+@editing.route("/", methods=['GET'])
+def edit_video():
+    return render_template('editing.html')
+
+
+@editing.route("/clips/<filename>")
+def render_clip(filename):
+    import os
+    path = os.getcwd() + video_save_path + filename
+    print(path)
+    return send_file(path)
+
+
+@editing.route("/upload", methods=['POST'])
+def upload_video():
+    # check if video save path exists
+    if not os.path.isdir("./clips"):
+        os.mkdir("./clips")
+    try:
+        video_file = request.files['videofile']
+        filepath = os.getcwd() + video_save_path + video_file.filename
+        print("*** Your filepath is: " + filepath + "***")
+        video_file.save(filepath)
+    except Exception as e:
+        print(e)
+
+    # Slice off the beginning of the filepath (os.getcwd())
+    return str(filepath[26:])
+
+
+# Main video editing pipeline
+@editing.route('/edit_video/<actiontype>', methods=['POST'])
+def editVideo(actiontype):
+    if actiontype == "trim":
+        try:
+            edited_videopath = trimVideo(request.form['videofile'], int(request.form['trim_start']),
+                                         int(request.form['trim_end']))
+            return {
+                "status": "success",
+                "message": "video edit success",
+                "edited_videopath": edited_videopath
+            }
+        except Exception as e:
+            return {
+                "status": "error",
+                "message": "video edit failure: " + str(e),
+            }
+    elif actiontype == "image":
+        print("image!")
+
+
+@editing.route('/merged_render', methods=['POST'])
+def mergedRender():
+    try:
+        videoscount = int(request.form['videoscount'])
+        if videoscount > 0:
+            videoclip_filenames = []
+            for i in range(videoscount):
+                videoclip_filenames.append(request.form['video' + str(i)])
+
+            finalrender_videopath = mergeVideos(videoclip_filenames)
+            return {
+                "status": "success",
+                "message": "merged render success",
+                "finalrender_videopath": finalrender_videopath
+            }
+        else:
+            return {
+                "status": "error",
+                "message": "merged render error. Invalid videos count"
+            }
+
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": "video merge failure: " + str(e),
+        }
--- index.py
+++ index.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, render_template, send_file
-from video_utils import *
+from flask import Flask, request, render_template, send_file, Blueprint
+
+import editing.routes
+from editing import routes
 import os
 
 # TODO: Fix the trimming not working.
@@ -18,8 +20,9 @@ import os
 # Last: Connect both ends
 
 # Change this to change the saved file path
-video_save_path = "\clips\\"
+
 app = Flask(__name__)  # name for flask app
+app.register_blueprint(editing.routes.editing, url_prefix='/editing')
 
 
 # welcome screen
@@ -31,85 +34,6 @@ def index():
 @app.route("/goals_and_guidelines")
 def goals():
     return render_template('tutorial.html')
-
-
-@app.route("/editing", methods=['GET'])
-def edit_video():
-    return render_template('editing.html')
-
-
-@app.route("/editing/clips/<filename>")
-def render_clip(filename):
-    import os
-    path = os.getcwd() + video_save_path + filename
-    print(path)
-    return send_file(path)
-
-
-@app.route("/editing/upload", methods=['POST'])
-def upload_video():
-    # check if video save path exists
-    if not os.path.isdir("./clips"):
-        os.mkdir("./clips")
-    try:
-        video_file = request.files['videofile']
-        filepath = os.getcwd() + video_save_path + video_file.filename
-        print("*** Your filepath is: " + filepath + "***")
-        video_file.save(filepath)
-    except Exception as e:
-        print(e)
-
-    # Slice off the beginning of the filepath (os.getcwd())
-    return str(filepath[26:])
-
-
-# Main video editing pipeline
-@app.route('/editing/edit_video/<actiontype>', methods=['POST'])
-def editVideo(actiontype):
-    if actiontype == "trim":
-        try:
-            edited_videopath = trimVideo(request.form['videofile'], int(request.form['trim_start']),
-                                         int(request.form['trim_end']))
-            return {
-                "status": "success",
-                "message": "video edit success",
-                "edited_videopath": edited_videopath
-            }
-        except Exception as e:
-            return {
-                "status": "error",
-                "message": "video edit failure: " + str(e),
-            }
-    elif actiontype == "image":
-        print("image!")
-
-
-@app.route('/editing/merged_render', methods=['POST'])
-def mergedRender():
-    try:
-        videoscount = int(request.form['videoscount'])
-        if videoscount > 0:
-            videoclip_filenames = []
-            for i in range(videoscount):
-                videoclip_filenames.append(request.form['video' + str(i)])
-
-            finalrender_videopath = mergeVideos(videoclip_filenames)
-            return {
-                "status": "success",
-                "message": "merged render success",
-                "finalrender_videopath": finalrender_videopath
-            }
-        else:
-            return {
-                "status": "error",
-                "message": "merged render error. Invalid videos count"
-            }
-
-    except Exception as e:
-        return {
-            "status": "error",
-            "message": "video merge failure: " + str(e),
-        }
 
 
 if __name__ == "__main__":
