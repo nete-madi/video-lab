6bf7a17b84f5be614c7fc0de2557d83fdfe567b8
Nadia Etemadi <netemadi20@gmail.com>
2022-10-22 16:03:46 -0700

    working on image upload

--- index.py
+++ index.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 import editing.routes
 
-# TODO: In editing.html: Configure the payload for image overlay.
-# TODO: In index.py: Configure the route for "image" actiontype.
+# TODO: In editing.html: Configure the payload for image overlay. Here is the payload and console output:
+# editor_payload
+# Object { img_src: undefined, start_time: "0", duration: "10", videofile: "clips\\sample-mp4-file.mp4" }
+# editing:317:13
+# video edit failure: 400 Bad Request: The browser (or proxy) sent a request that this server could not understand.
 # TODO: merged_render editing route is not working
 
 # https://stackoverflow.com/questions/72914568/overlay-image-on-video-using-moviepy
-# First: implement frontend for the functionality.
-# Next: implement the functionality itself in video_utils.py
-# Last: Connect both ends
 
 app = Flask(__name__)  # name for flask app
 app.register_blueprint(editing.routes.editing, url_prefix='/editing')
