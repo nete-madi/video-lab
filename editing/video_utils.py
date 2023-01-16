89a63ddcafd70f7673e641540fe93014ab9ddb20
Nadia Etemadi <netemadi20@gmail.com>
2023-01-16 15:46:32 -0800

    removed opencv going to try something else

--- editing/video_utils.py
+++ editing/video_utils.py
@@ -1,6 +1,5 @@
 import os
 import time
-import cv2
 from moviepy.editor import VideoFileClip, concatenate_videoclips, ImageClip, CompositeVideoClip
 
 video_save_path = os.getcwd() + "\\editing\\clips\\"
@@ -17,9 +16,6 @@ def trim_video(videofile: str, start_time: int, end_time: int):
 
 def img_overlay(videofile: str, img: str, start_time: int, duration: int, x_pos: int, y_pos: int):
     clip = VideoFileClip(videofile)
-    vid = cv2.VideoCapture(clip)
-    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
     img = ImageClip(img).set_start(start_time).set_duration(duration).set_pos((x_pos, y_pos))
     edited_path = video_save_path + "edited_" + str(int(time.time())) + videofile[41:]
     final = CompositeVideoClip([clip, img])
