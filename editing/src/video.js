a2b3f4bc5857c925bfa3039b18e4f81d64f62e01
Nadia Etemadi <netemadi20@gmail.com>
2023-01-17 13:35:46 -0800

    moved drag logic into video.js

--- editing/src/video.js
+++ editing/src/video.js
@@ -1,3 +1,91 @@
+let isDragging = false;
+var shapeTop;
+var shapeLeft;
+document.addEventListener('mousedown', function (event) {
+
+    let dragElement = event.target.closest('.draggable');
+    var editingArea = document.querySelector("#editArea");
+    var circle = document.querySelector("#circle");
+
+    dragElement.ondragstart = function () {
+        return false;
+    };
+
+    let shiftX = event.clientX - dragElement.getBoundingClientRect().left;
+    let shiftY = event.clientY - dragElement.getBoundingClientRect().top;
+
+    dragElement.style.position = 'absolute';
+    dragElement.style.zIndex = 1000;
+    document.body.append(dragElement);
+
+    moveAt(event.pageX, event.pageY);
+
+    // moves the dragElement at (pageX, pageY) coordinates
+    // taking initial shifts into account
+    function moveAt(pageX, pageY) {
+        dragElement.style.left = pageX - shiftX + 'px';
+        dragElement.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+    }
+
+    // move the dragElement on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+
+    // drop the dragElement, remove unneeded handlers
+    dragElement.onmouseup = function () {
+        document.removeEventListener('mousemove', onMouseMove);
+        dragElement.onmouseup = null;
+
+        // Get the top, left coordinates of two elements
+        const shapeArea = circle.getBoundingClientRect();
+        const edArea = editingArea.getBoundingClientRect();
+
+        // Calculate the top and left positions
+        const top = shapeArea.top - edArea.top;
+        const left = shapeArea.left - edArea.left;
+
+        console.log("top: " + top);
+        console.log("left: " + left);
+
+        /*
+
+        let Left1 = editingArea.offsetLeft; // undefined
+        let Left2 = circle.offsetLeft;
+        let Width1 = $("#editArea").outerWidth();
+        let Width2 = $("#circle").width();
+        let Top1 = editingArea.offsetTop; // undefined
+        let Top2 = circle.offsetTop;
+        let Height1 = $("#editArea").outerHeight();
+        let Height2 = $("#circle").height();
+
+
+        console.log("left1: " + Left1);
+        console.log("left2: " + Left2);
+        console.log("width1: " + Width1);
+        console.log("Width2: " + Width2);
+        console.log("Top1: " + Top1);
+        console.log("Top2: " + Top2);
+        console.log("Height1: " + Height1);
+        console.log("Height2: " + Height2);
+
+
+        if( ((Left1 + Width1) >= Left2)
+        && (Left1 <= (Left2 + Width2))
+        && ((Top1 + Height1) >= Top2)
+        && (Top1 <= (Top2 + Height2))) {
+            console.log("in bounds");
+        }
+        else {
+            console.log("not in bounds");
+        }
+        */
+    };
+}); // https://javascript.info/mouse-drag-and-drop
+
+
 function updateProgressBar(percent) {
 	$("#uploadprogress").css('width', percent + "%");
 	$("#uploadprogress").html(percent + "%");
@@ -108,8 +196,8 @@ var app = new Vue({
 						start_time: 0,
 						//$("#start_stamp"+videoID).val(),
 						duration: 10,
-						x_pos:100,
-						y_pos:100
+						x_pos:shapeLeft,
+						y_pos:shapeTop
 						//$("#duration"+videoID).val(),
 						//x_pos: event.clientX,
 						//y_pos: event.clientY,
